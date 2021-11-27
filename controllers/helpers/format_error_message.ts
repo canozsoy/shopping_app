b4d4e1357c9e989654f7cd29e75ee841b6227df6
canozsoy <cnozsy@gmail.com>
2021-11-27 13:08:50 +0300

    error messages for db is created, error messages for validations is created

--- controllers/helpers/format_error_message.ts
+++ controllers/helpers/format_error_message.ts
@@ -4,6 +4,22 @@ const formatString = (val:string):string => val.replace(/"/g, '');
 
 const firstCaseUpper = (val:string):string => val.charAt(0).toUpperCase() + val.slice(1);
 
-const formatErrorMessage = (error:ValidationError) => error.details.map((x) => firstCaseUpper(formatString(x.message)));
+function formatErrorValidationMessage(error:ValidationError) {
+  return error.details.map((x) => ({
+    message: firstCaseUpper(formatString(x.message)),
+    key: x?.context?.key,
+  }));
+}
 
-export default formatErrorMessage;
+function formatErrorDBMessage(error:any) {
+  return error.errors.map((x:any) => ({
+    message: firstCaseUpper(x.message),
+    key: x.path,
+    value: x.value,
+  }));
+}
+
+export {
+  formatErrorValidationMessage,
+  formatErrorDBMessage,
+};
