4f07fe945d039a38f96f6f2113aab951299c9772
canozsoy <cnozsy@gmail.com>
2021-11-27 15:53:40 +0300

    authentication

--- routes/customer.ts
+++ routes/customer.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import customerController from '../controllers/customer_controller';
+import { verifyJWT } from '../strategies/jwt';
 
 const router = express.Router();
+
+router.use(verifyJWT);
 
 router.route('/:customerId/order')
   .get(customerController.listAllOrders)
--- strategies/jwt.ts
+++ strategies/jwt.ts
@@ -1,4 +1,6 @@
 import jwt from 'jsonwebtoken';
+import { NextFunction, Request, Response } from 'express';
+import CustomErrorObject from './error_object';
 
 const createJWT = (username: string, role:string):string => {
   const jwtSecret = process.env.JWT_SECRET as string;
@@ -8,11 +10,44 @@ const createJWT = (username: string, role:string):string => {
   });
 };
 
-const validateJWT = () => {
-  console.log('asdf');
+const extractJWTFromBearerToken = (req: Request):string | null => {
+  const bearerToken = req.headers.authorization;
+  if (!bearerToken || !bearerToken.includes('Bearer ')) {
+    return null;
+  }
+  return bearerToken.replace('Bearer ', '');
+};
+
+const verifyJWT = async (req: Request, res: Response, next:NextFunction) => {
+  const token = extractJWTFromBearerToken(req);
+  if (!token) {
+    return next(new CustomErrorObject([{ message: 'Authentication failed' }], 401));
+  }
+  let payload;
+  const jwtSecret = process.env.JWT_SECRET as string;
+  try {
+    payload = await jwt.verify(token, jwtSecret);
+  } catch (err) {
+    return next(new CustomErrorObject([{ message: 'Authentication failed' }], 401));
+  }
+  payload = payload as object;
+  res.locals = {
+    user: payload,
+  };
+  return next();
+};
+
+const adminVerifyJWT = (req: Request, res: Response, next: NextFunction) => {
+  const user = res?.locals?.user;
+  if (user.role !== 'admin') {
+    return next(new CustomErrorObject([{ message: 'Authentication failed' }], 403));
+  }
+  return next();
 };
 
 export {
   createJWT,
-  validateJWT,
+  verifyJWT,
+  extractJWTFromBearerToken,
+  adminVerifyJWT,
 };
