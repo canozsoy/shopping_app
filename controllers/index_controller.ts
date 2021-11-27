51c27a7b9088fd4403a535992b1e652a12bb8b40
canozsoy <cnozsy@gmail.com>
2021-11-27 13:51:22 +0300

    login post is implemented

--- controllers/index_controller.ts
+++ controllers/index_controller.ts
@@ -1,17 +1,58 @@
 import { Request, Response, NextFunction } from 'express';
 import bcrypt from 'bcrypt';
-import signupValidations from './validations/validations';
+import userValidations from './validations/validations';
 import { formatErrorValidationMessage, formatErrorDBMessage } from './helpers/format_error_message';
 import { User } from '../models';
 import CustomErrorObject from '../strategies/error_object';
 import { createJWT } from '../strategies/jwt';
 
-const loginPost = (req : Request, res : Response) => {
-  res.sendStatus(200);
+const loginPost = async (req : Request, res : Response, next: NextFunction) => {
+  const validationResult = userValidations.validate(req.body, { abortEarly: false });
+
+  if (validationResult.error) {
+    const { error } = validationResult;
+    const messages = formatErrorValidationMessage(error);
+    const errorObj = new CustomErrorObject(messages, 400);
+    return next(errorObj);
+  }
+
+  const { username, password } = req.body;
+  let currentUser;
+  try {
+    currentUser = await User.findOne({ where: { username } });
+  } catch (err) {
+    const messages = formatErrorDBMessage(err);
+    return next(new CustomErrorObject(messages));
+  }
+  if (!currentUser) {
+    return next(new CustomErrorObject({ message: 'Incorrect username or password' }, 401));
+  }
+
+  let passwordValidation;
+  try {
+    passwordValidation = await bcrypt.compare(password, currentUser.password);
+  } catch (err) {
+    return next(new CustomErrorObject({ message: 'Internal server error' }));
+  }
+  if (!passwordValidation) {
+    return next(new CustomErrorObject({ message: 'Incorrect username or password' }, 401));
+  }
+
+  const token = createJWT(username, currentUser.role);
+  return res.json({
+    message: 'Successfully Logged In',
+    currentUser: {
+      id: currentUser.id,
+      username: currentUser.username,
+      role: currentUser.role,
+      jwt: token,
+    },
+
+  });
 };
 
 const signupPost = async (req : Request, res : Response, next: NextFunction) => {
-  const validationResult = signupValidations.validate(req.body, { abortEarly: false });
+  const validationResult = userValidations.validate(req.body, { abortEarly: false });
   if (validationResult.error) {
     const { error } = validationResult;
     const messages = formatErrorValidationMessage(error);
@@ -40,13 +81,14 @@ const signupPost = async (req : Request, res : Response, next: NextFunction) => 
     return next(errorObj);
   }
 
-  const token = createJWT(username);
+  const token = createJWT(username, newUser.role);
 
   return res.json({
     message: 'Successfully Created',
     newUser: {
       id: newUser?.id,
       username: newUser.username,
+      role: newUser.role,
       jwt: token,
     },
   });
