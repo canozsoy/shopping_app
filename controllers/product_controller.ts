4a051760d9d24ed09eee3cd260a1341620ea6e26
canozsoy <cnozsy@gmail.com>
2021-11-28 15:23:47 +0300

    product endpoint

--- controllers/product_controller.ts
+++ controllers/product_controller.ts
@@ -1,11 +1,48 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { Product } from '../models';
+import { formatErrorDBMessage, formatErrorValidationMessage } from './helpers/format_error_message';
+import CustomErrorObject from '../strategies/error_object';
+import { idValidation } from './validations/validations';
 
-const listAllProducts = (req : Request, res : Response) => {
-  res.sendStatus(200);
+const listAllProducts = async (req : Request, res : Response, next:NextFunction) => {
+  let products;
+  try {
+    products = await Product.findAll({});
+  } catch (err) {
+    const messages = formatErrorDBMessage(err);
+    return next(new CustomErrorObject(messages));
+  }
+  if (!products.length) {
+    return next(new CustomErrorObject([{ message: 'No product found' }], 404));
+  }
+  return res.json({
+    message: 'Successfully Found',
+    products,
+  });
 };
 
-const getProduct = (req : Request, res : Response) => {
-  res.sendStatus(200);
+const getProduct = async (req : Request, res : Response, next:NextFunction) => {
+  const { id } = req.params;
+  const idValidationResult = idValidation.validate(id);
+  if (idValidationResult.error) {
+    const messages = formatErrorValidationMessage(idValidationResult.error);
+    return next(new CustomErrorObject(messages, 400));
+  }
+
+  let product;
+  try {
+    product = await Product.findOne({ where: { id } });
+  } catch (err) {
+    const messages = formatErrorDBMessage(err);
+    return next(new CustomErrorObject(messages));
+  }
+  if (!product) {
+    return next(new CustomErrorObject([{ message: 'Product not found' }], 404));
+  }
+  return res.json({
+    message: 'Successfully Found',
+    product,
+  });
 };
 
 export default {
